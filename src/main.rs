c94a128a63c92a183a0dd1a107ad266d08ab6780
TejaBeta <me@tejathota.dev>
2026-09-15 21:56:26 +0100

    Add bin

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@
 
 use core::panic::PanicInfo;
 
-#[panic_handler]
-fn panic(_info: &PanicInfo) -> ! {
+#[unsafe(no_mangle)]
+pub extern "C" fn _start() -> ! {
+    // this function is the entry point, since the linker looks for a function
+    // named `_start` by default
     loop{}
 }
 
-#[unsafe(no_mangle)]
-pub extern "C" fn _start() -> ! {
+/// This function is called on panic.
+#[panic_handler]
+fn panic(_info: &PanicInfo) -> ! {
     loop{}
 }
